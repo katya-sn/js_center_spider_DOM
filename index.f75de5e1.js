63eb743f3c7ce1aa4257f5e858d35f6bd52b1869
katya-sn <kattya.sagun@gmail.com>
2024-05-14 16:00:51 +0300

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,9 @@
+"use strict";
+const field = document.querySelector(".wall");
+const spider = document.querySelector(".spider");
+const height = (field.clientHeight - spider.clientHeight) / 2;
+const horizontal = (field.clientWidth - spider.clientWidth) / 2;
+spider.style.top = `${height}px`;
+spider.style.left = `${horizontal}px`;
+
+//# sourceMappingURL=index.f75de5e1.js.map
